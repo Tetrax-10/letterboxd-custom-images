cc24dcd8129f5904c330c53cee69d3764cd0c0ff
Raghavan <raghavanmarch2002@gmail.com>
2024-09-24 23:52:55 +0530

    3.8

--- lci.user.js
+++ lci.user.js
@@ -3,7 +3,7 @@
 // @description  Customize letterboxd posters and backdrops without letterboxd PATRON
 // @author       Tetrax-10
 // @namespace    https://github.com/Tetrax-10/letterboxd-custom-images
-// @version      3.7
+// @version      3.8
 // @license      MIT
 // @match        *://*.letterboxd.com/*
 // @connect      themoviedb.org
